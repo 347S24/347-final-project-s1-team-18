81eab9ceb2916639118e0ed290efc17398a972dd
Colin Gregory <gregorycolin08@gmail.com>
2024-04-17 23:24:21 -0400

    Some new database stuff as well as seeing distances between 2 locations for now. Will be useful for the future TSP alg. Also added a bit of google maps embedded stuff. Dont know how useful it will be right now but i thought it would be cool to just see it.

--- localsights/localsights/maps/views.py
+++ localsights/localsights/maps/views.py
@@ -40,7 +40,7 @@ from datetime import datetime
 #         for a in eligable_locations: 
 #             data = {
 #                 'lat': float(a.lat), 
-#                 'lng': float(a.lng), 
+#                   lng': float(a    lng), 
 #                 'name': a.name
 #             }
 
@@ -74,11 +74,11 @@ class GeocodingView(View):
     def get(self,request,pk): 
         location = Location.objects.get(pk=pk)
 
-        # if location.lng and location.lat and location.place_id != None: 
-        #     lat = location.lat
-        #     lng = location.lng
-        #     place_id = location.place_id
-        #     label = "from my database"
+        if location.lng and location.lat and location.place_id != None: 
+            lat = location.lat
+            lng = location.lng
+            place_id = location.place_id
+            label = "from my database"
 
         if location.address and location.country and location.zipcode and location.city != None: 
             address_string = str(location.address)+", "+str(location.zipcode)+", "+str(location.city)+", "+str(location.country)
@@ -89,27 +89,106 @@ class GeocodingView(View):
             lat = result.get('geometry', {}).get('location', {}).get('lat', None)
             lng = result.get('geometry', {}).get('location', {}).get('lng', None)
             place_id = result.get('place_id', {})
-            # label = "from my api call"
-            # location.lat = lat
-            # location.lng = lng
-            # location.place_id = place_id
-            # location.save()
+            label = "from my api call"
+            location.lat = lat
+            location.lng =  lng
+            location.place_id = place_id
+            location.save()
 
         else: 
             result = ""
-            # lat = ""
-            # lng = ""
-            # place_id = ""
-            # label = "no call made"
+            lat = ""
+            lng = ""
+            place_id = ""
+            label = "no call made"
 
 
         context = {
+            'result':result,
             'location':location,
-            # 'lat':lat, 
-            # 'lng':lng, 
-            # 'place_id':place_id, 
-            # 'label':label
+            'lat':lat, 
+            'lng':lng, 
+            'place_id':place_id, 
+            'label':label
         }
         
         return render(request, self.template_name, context)
+    
+class DistanceView(View):
+    template_name = "pages/distance.html"
+
+    def get(self, request): 
+        form = DistanceForm
+        distances = Distances.objects.all()
+        context = {
+            'form':form,
+            'distances':distances
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request): 
+        form = DistanceForm(request.POST)
+        if form.is_valid(): 
+            from_location = form.cleaned_data['from_location']
+            from_location_info = Location.objects.get(name=from_location)
+            from_address_string = str(from_location_info.address)+", "+str(from_location_info.zipcode)+", "+str(from_location_info.city)+", "+str(from_location_info.country)
+
+            to_location = form.cleaned_data['to_location']
+            to_location_info = Location.objects.get(name=to_location)
+            to_address_string = str(to_location_info.address)+", "+str(to_location_info.zipcode)+", "+str(to_location_info.city)+", "+str(to_location_info.country)
+
+            mode = form.cleaned_data['mode']
+            now = datetime.now()
+            key = getattr(settings, 'GOOGLE_API_KEY', None)
+            gmaps = googlemaps.Client(key)
+            calculate = gmaps.distance_matrix(
+                    from_address_string,
+                    to_address_string,
+                    mode = mode,
+                    departure_time = now
+            )
+
+
+            duration_seconds = calculate['rows'][0]['elements'][0]['duration']['value']
+            duration_minutes = duration_seconds/60
+
+            distance_meters = calculate['rows'][0]['elements'][0]['distance']['value']
+            distance_kilometers = distance_meters/1000
+
+            if 'duration_in_traffic' in calculate['rows'][0]['elements'][0]: 
+                duration_in_traffic_seconds = calculate['rows'][0]['elements'][0]['duration_in_traffic']['value']
+                duration_in_traffic_minutes = duration_in_traffic_seconds/60
+            else: 
+                duration_in_traffic_minutes = None
+
+            
+            obj = Distances(
+                from_location = Location.objects.get(name=from_location),
+                to_location = Location.objects.get(name=to_location),
+                mode = mode,
+                distance_km = distance_kilometers,
+                duration_mins = duration_minutes,
+                duration_traffic_mins = duration_in_traffic_minutes
+            )
+
+            obj.save()
+
+        else: 
+            print(form.errors)
+        
+        return redirect('distance')
+    
+class DisplayMapView(View): 
+    template_name = "pages/display_map.html"
+    apikey = getattr(settings, 'GOOGLE_API_KEY', None)
+    def get(self,request): 
+        context = {
+            "key": DisplayMapView.apikey
+        }
+
+        return render(request, self.template_name, context)
+
+
+    
     
