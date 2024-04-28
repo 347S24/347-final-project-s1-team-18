f4538883071857c5f7ce5053370064c602d8d195
Brock Redman <brockdarock21@gmail.com>
2024-04-28 13:17:11 -0400

    Location burg fixed

--- localsights/localsights/maps/models.py
+++ localsights/localsights/maps/models.py
@@ -16,8 +16,8 @@ class Location(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     edited_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
-    latitude = models.DecimalField(max_digits = 9, decimal_places = 6, blank=True, null=True) 
-    longitude = models.DecimalField(max_digits = 9, decimal_places = 6, blank=True, null=True) 
+    lat = models.DecimalField(max_digits = 9, decimal_places = 6, blank=True, null=True) 
+    lng = models.DecimalField(max_digits = 9, decimal_places = 6, blank=True, null=True) 
     place_id = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
